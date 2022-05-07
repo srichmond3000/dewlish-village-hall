e553d9fe8b417200cda22c523714f69daa212ca2
srichmond3000 <srichmond3000@googlemail.com>
2022-05-07 11:59:52 +0100

    Add Header component and move components folder

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Header from '../components/Header';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+    </>
+  );
 };
 
 export default Home;
